badc12bfcda72db0576637307a9a3f2505af5c76
inul rifaya <inulrifaya2006@gmail.com>
2026-07-21 15:03:29 +0530

    Allow frontend domain

--- backend/bloodbank/settings.py
+++ backend/bloodbank/settings.py
@@ -110,6 +110,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://blood-donation-frontend-irnb.onrender.com",
 ]
 
 CORS_ALLOW_METHODS = [
